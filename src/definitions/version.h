bc3f88dc5bb7161c60dd6c2f60de6ac6f0601d86
s.potapov <s.potapov@27819519-1eb6-1c4a-a37e-3b2cdee9ef29>
2011-12-10 16:53:44 +0000

    *0.5.1*

--- src/definitions/version.h
+++ src/definitions/version.h
@@ -3,7 +3,7 @@
 
 #define CLIENT_GUID             "{9732304B-B640-4C54-B2CD-3C2297D649A1}"
 #define CLIENT_NAME             "Contacts"
-#define CLIENT_VERSION          "0.5.0"
+#define CLIENT_VERSION          "0.5.1"
 #define CLIENT_VERSION_SUFIX    "beta"
 #define CLIENT_HOME_PAGE        "http://contacts.rambler.ru"
 
