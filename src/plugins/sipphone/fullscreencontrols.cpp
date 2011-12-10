574985004484c4c902a2aa279550711af946c1f1
s.potapov <s.potapov@27819519-1eb6-1c4a-a37e-3b2cdee9ef29>
2011-12-10 14:23:27 +0000

    Обновлены иконки входящего и пропущенного звонков.

--- src/plugins/sipphone/fullscreencontrols.cpp
+++ src/plugins/sipphone/fullscreencontrols.cpp
@@ -22,9 +22,8 @@ FullScreenControls::FullScreenControls( QWidget *parent)
 	connect(ui.wgtAVControl, SIGNAL(micStateChange(bool)), this, SIGNAL(micStateChange(bool)));
 	connect(ui.wgtAVControl, SIGNAL(micVolumeChange(int)), this, SIGNAL(micVolumeChange(int)));
 
-	QImage imgHangup = iconStorage->getImage(MNI_SIPPHONE_WHITE_HANGUP);
 	QIcon iconHangup;
-	iconHangup.addPixmap(QPixmap::fromImage(imgHangup), QIcon::Normal, QIcon::On);
+	iconHangup.addPixmap(QPixmap::fromImage(iconStorage->getImage(MNI_SIPPHONE_WHITE_HANGUP)), QIcon::Normal, QIcon::On);
 	ui.btnHangup->setIcon(iconHangup);
 	connect(ui.btnHangup, SIGNAL(clicked()), this, SIGNAL(hangup()));
 
